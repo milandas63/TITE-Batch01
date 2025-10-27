9e0abb478cad61c3c16674f26188f73485361b94
Milan Das <45796126+milandas63@users.noreply.github.com>
2025-10-27 12:41:46 +0530

    Add files via upload

--- Day-06 (27-10-2025)/Line.java
+++ Day-06 (27-10-2025)/Line.java
@@ -58,6 +58,26 @@ class Line {
 			}
 		}
 
+
+		/*
+					1
+				   121
+				  12321
+				 1234321
+				123454321
+			   12345654321
+			  1234567654321
+			 123456787654321
+			12345678987654321
+		*/
+		spaces = 20;
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=spaces; j++) System.out.print(" ");
+			for(int j=1; j<=i; j++)      System.out.print(j);
+			for(int j=(i-1); j>=1; j--)  System.out.print(j);
+			System.out.println();
+			spaces--;
+		}
 	}
 
 }
